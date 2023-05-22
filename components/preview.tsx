8dd36b0b0960fdb38f00b36f2d88571f15a07e02
Daniel Bodky <dbodky@gmail.com>
2023-05-22 21:01:35 +0200

    Updates ratios

--- components/preview.tsx
+++ components/preview.tsx
@@ -22,34 +22,35 @@ export default function Preview (props: PreviewProps) {
         <div id="preview" className="flex flex-col justify-between gap-4 w-[874px] h-[1240px] bg-light text-dark">
             <Image id="preview-background" src={ backgroundImage } alt=""/>
             <div id="preview-content" className="flex flex-col justify-between gap-4 w-[874px] h-[1240px] bg-transparent text-dark py-4">
-            <input type="text" id="mocName" placeholder="MOC title" className={ "mx-auto w-4/5 text-6xl text-center uppercase " + robotoBold.className}></input> 
+                <input type="text" id="mocName" placeholder="MOC title" className={ "mx-auto w-4/5 h-[196px] text-7xl text-center uppercase " + robotoBold.className}></input> 
 
-            <div className="h-1 mx-auto w-4/5 bg-brand"/>
-             
-            <textarea className={"grow mx-auto w-4/5 text-4xl resize-none " + roboto.className} id="mocDescription" name="mocDescription" placeholder={ loremIpsum }/>
+                <div className="h-1 mx-auto w-4/5 bg-brand"/>
 
-            <div className="h-1 mx-auto w-4/5 bg-brand"></div>
+                <textarea className={"grow mx-auto w-4/5 text-4xl h-[562px] resize-none " + roboto.className} id="mocDescription" name="mocDescription" placeholder={ loremIpsum }/>
 
-            <div className="flex mx-auto w-4/5 flex-row gap-16 ">
-                <Image className="ml-8 mb-8 rounded-full h-[160px] w-[160px] ring-dark" src={ avatarPNG } width={200} height={200} alt="Profile picture"/>
-                
-                <div className="flex mb-6 flex-col">
-                    <input className={"text-4xl " + robotoBold.className} type="text" id="builder" name="builder" placeholder="Builder"/>
-                    
-                    <input className={"text-3xl " + robotoBold.className } type="text" id="from" name="from" placeholder="Billund"/>
+                <div className="h-1 mx-auto w-4/5 bg-brand"></div>
 
-                    <div className="flex flex-row gap-2 mt-4 mb-2">
-                        <Image className="border-4 rounded-lg border-dark" width={30} height={30} src={ flickrSVG } alt="Flickr Logo"/>
-                        <input className={"text-2xl " + roboto.className } type="text" id="flickr" name="flickr" placeholder="username"/>
-                    </div>
+                <div className="flex mx-auto w-4/5 flex-row pt-8 gap-16 h-[256px]">
+                    <Image className="ml-8 mb-8 rounded-full h-[160px] w-[160px] ring-dark" src={ avatarPNG } width={200} height={200} alt="Profile picture"/>
 
-                    <div className="flex flex-row gap-2">
-                        <Image width={30} height={30} src={ instagramSVG } alt="Instagram Logo"/>
-                        <input className={"text-2xl " + roboto.className } type="text" id="instagram" name="instagram" placeholder="username"/>
+                    <div className="flex mb-6 flex-col">
+                        <input className={"text-4xl " + robotoBold.className} type="text" id="builder" name="builder" placeholder="Builder"/>
+
+                        <input className={"text-3xl " + robotoBold.className } type="text" id="from" name="from" placeholder="Billund"/>
+
+                        <div className="flex flex-row gap-2 mt-4 mb-2">
+                            <Image className="border-4 rounded-lg border-dark" width={30} height={30} src={ flickrSVG } alt="Flickr Logo"/>
+                            <input className={"text-2xl " + roboto.className } type="text" id="flickr" name="flickr" placeholder="username"/>
+                        </div>
+
+                        <div className="flex flex-row gap-2">
+                            <Image width={30} height={30} src={ instagramSVG } alt="Instagram Logo"/>
+                            <input className={"text-2xl " + roboto.className } type="text" id="instagram" name="instagram" placeholder="username"/>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <Image className="max-h-1/4 my-auto pb-10 justify-self-center self-center" width={300} src={ props.lug == "Roguebricks" ? roguebricksSVG : rebellugSVG } alt="Roguebricks Logo"></Image>
+
+                <Image className="max-h-1/4 pb-10 h-[206px] justify-self-center self-center" width={300} src={ props.lug == "Roguebricks" ? roguebricksSVG : rebellugSVG } alt="Roguebricks Logo"></Image>
         
             </div>
         </div>  
