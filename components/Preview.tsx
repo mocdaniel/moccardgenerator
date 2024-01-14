36b7f41e58977c53600817a41d59657a683abd86
Daniel Bodky <dbodky@gmail.com>
2024-01-14 23:23:45 +0100

    Renames Roguebricks SVG component

--- components/Preview.tsx
+++ components/Preview.tsx
@@ -12,7 +12,7 @@ import { Color } from "react-color-palette"
 import { LinkDropdown } from './LinkDropdown'
 import FooterDropdown from "./FooterDropdown"
 import RebellugSVG from "./ui/rebellug-svg"
-import RoguebricksSVG from "./ui/roguebricks-menu"
+import RoguebricksSVG from "./ui/roguebricks-svg"
 import { Button } from "./ui/button"
 import { ImagePlus } from "lucide-react"
 
@@ -165,7 +165,8 @@ export default function Preview (props: PreviewProps) {
                         src={URL.createObjectURL(footerImage)} alt="Logo"/> :
 (showRogueLight ? <RoguebricksSVG dark={false}/> : 
                                 ( showRogueDark ? <RoguebricksSVG dark={true}/> : 
-                                    ( showRebelLight ? <RebellugSVG dark={false} /> : <RebellugSVG dark={true} />)))
+                                    ( showRebelLight ? <RebellugSVG dark={false} /> : 
+                                        ( showRebelDark ? <RebellugSVG dark={true} /> : <></>))))
                      }
                      </div>
             </div>
