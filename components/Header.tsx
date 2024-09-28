9f9dab2799846bf87233b851976bdba719d74ed3
Daniel Bodky <dbodky@gmail.com>
2024-09-28 12:50:01 -0400

    Update Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,28 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
-import {FileDownIcon } from "lucide-react";
+import { FileDownIcon } from "lucide-react";
 
 type HeaderProps = {
-    renderImage: (() => void) | null;
-}
+  renderImage: (() => void) | null;
+};
 export default function Header(props: HeaderProps) {
-    return (
-        <div className="flex flex-row w-full h-8 justify-between py-4 px-8 items-baseline text-white">
-            <span className="text-2xl">MOCCardGenerator</span>
-            <div className="flex flex-row justify-between items-center gap-8">
-                <Link className="hover:underline text-xl" href={"/"}>Home</Link>
-                <Link className="hover:underline text-xl" href={"/faq"}>FAQ</Link>
-                { props.renderImage && 
-                    <Button onClick={props.renderImage} variant="affirmative">
-                        <FileDownIcon className="mr-2"/>
-                        Download PDF
-                    </Button>
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-row w-full h-8 justify-between py-4 px-8 items-baseline text-white">
+      <span className="text-2xl">Brick.cards</span>
+      <div className="flex flex-row justify-between items-center gap-8">
+        <Link className="hover:underline text-xl" href={"/"}>
+          Home
+        </Link>
+        <Link className="hover:underline text-xl" href={"/faq"}>
+          FAQ
+        </Link>
+        {props.renderImage && (
+          <Button onClick={props.renderImage} variant="affirmative">
+            <FileDownIcon className="mr-2" />
+            Download PDF
+          </Button>
+        )}
+      </div>
+    </div>
+  );
 }
