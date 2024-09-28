3502ddcd55ef79d2b4c9f9fb6ba39da833bb2380
Daniel Bodky <dbodky@gmail.com>
2024-09-28 12:59:21 -0400

    Update FAQ

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>MOC Card Generator</title>
+        <title>Brick.cards</title>
         <meta name="description" content="Simple generator of MOC cards" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
